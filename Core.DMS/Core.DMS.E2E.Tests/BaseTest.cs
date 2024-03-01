5abd9b82d6a034f6be916ad9314eb7e34414b9ba
Callum Marshall <callummarshall9@googlemail.com>
2024-03-01 07:16:09 +0000

    ACCEPTANCE: Add acceptance tests for [DMS].[MoveFolderToFolder]

---
+++ Core.DMS/Core.DMS.E2E.Tests/BaseTest.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SqlServer.Dac;
+using System;
+
+namespace Core.DMS.E2E.Tests
+{
+    public class BaseTest
+    {
+        public int AppId { get; set; }
+        public string UserId { get; set; } = "test.user";
+
+        private string databaseName;
+        public SqlTransaction Transaction;
+        public SqlConnection Connection;
+
+        public void Setup()
+        {
+            databaseName = $"cCoderCore-{DateTimeOffset.Now.Ticks}";
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("TestServer");
+
+            Connection = new SqlConnection(connectionString);
+
+            var svc = new DacServices(connectionString);
+
+            var deploymentOptions = new DacDeployOptions
+            {
+                CreateNewDatabase = true,
+            };
+
+            svc.Deploy(DacPackage.Load(System.IO.Directory.GetCurrentDirectory() + "/cCoderCore-DMS-Module.dacpac"), databaseName, true, deploymentOptions);
+
+            Connection.Open();
+            Transaction = Connection.BeginTransaction();
+
+            Connection.Execute($@"USE [{databaseName}]", transaction: Transaction);
+
+            AppId = Connection.ExecuteScalar<int>(
+                sql: "INSERT INTO [CMS].[Apps] ([Domain]) VALUES (''); SELECT SCOPE_IDENTITY();",
+                transaction: Transaction);
+
+        }
+
+        public void Cleanup()
+        {
+            Transaction.Rollback();
+
+            Connection.Execute($@"USE [master];
+ALTER DATABASE [{databaseName}] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE [{databaseName}];
+EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'{databaseName}';");
+            Connection.Close();
+        }
+    }
+}
---
+++ Core.DMS/Core.DMS.E2E.Tests/DMSMoveStoredProcedureTests.cs
@@ -0,0 +1,156 @@
+﻿using Core.DMS.Objects.Entities;
+using Dapper;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace Core.DMS.E2E.Tests
+{
+    public class DMSMoveStoredProcedureTests : BaseTest
+    {
+        [Fact]
+        public void ShouldMoveFolderWithFileIntoFolderWithoutFile_Then_CreateFile()
+        {
+            //given
+            Setup();
+
+            Connection.Execute(
+                sql: $@"
+EXECUTE [DMS].[BuildFolderPath] 
+   {AppId}
+  ,'test/foo/bar';
+
+EXECUTE [DMS].[BuildFolderPath] 
+   {AppId}
+  ,'test/bar';
+
+EXECUTE [DMS].[CreateFile] 
+   '{UserId}'
+  ,'test.txt'
+  ,'test/bar'
+  ,{AppId}
+  ,'text/plain'
+  ,'0 B'
+  ,0x2;
+", transaction: Transaction);
+
+            var expectedFile = new File
+            {
+                CreatedBy = UserId,
+                Description = null,
+                MimeType = "text/plain",
+                Name = "test.txt",
+                Path = "test/foo/bar/test.txt",
+                Size = "0 B"
+            };
+
+            var expectedFiles = new[] { expectedFile };
+
+            //when
+            Connection.Execute(sql: $@"
+EXECUTE [DMS].[MoveFolderToFolder] 
+   '{UserId}'
+  ,{AppId}
+  ,'test/bar'
+  ,'test/foo';",
+  transaction: Transaction);
+
+            var actualFolders = Connection.Query<Folder>($"SELECT * FROM [DMS].[Folders]", transaction: Transaction);
+            var actualFiles = Connection.Query<File>("SELECT * FROM [DMS].[Files]", transaction: Transaction);
+            var actualFileContents = Connection.Query<FileContent>("SELECT * FROM [DMS].[FileContents]", transaction: Transaction);
+
+            //then
+            Cleanup();
+
+            Assert.True(actualFolders.Count() == 3
+                && actualFolders.Any(f => f.Path == "test")
+                && actualFolders.Any(f => f.Path == "test/foo")
+                && actualFolders.Any(f => f.Path == "test/foo/bar"));
+
+            //Files should've been merged into one.
+            var correctFolder = actualFolders.First(f => f.Path == "test/foo/bar");
+            expectedFile.FolderId = correctFolder.Id;
+
+            Assert.True(actualFiles.Count() == 1);
+
+            actualFiles.Should().BeEquivalentTo(expectedFiles, opts => opts.Excluding(e => e.CreatedOn).Excluding(e => e.Id));
+
+            Assert.True(actualFiles.First().FolderId == correctFolder.Id);
+            Assert.True(actualFiles.First().FolderId == correctFolder.Id);
+
+            var contentFileIds = actualFileContents.Select(c => c.FileId).Distinct();
+
+            Assert.True(contentFileIds.Count() == 1);
+
+            //Verify contents
+            Assert.Contains(actualFileContents, c => c.Version == 1 && c.RawData[0] == 0x02);
+        }
+
+        [Fact]
+        public void ShouldMoveFolderWithFileIntoFolderWithSameFileName_Then_MergeResults()
+        {
+            //given
+            Setup();
+
+            Connection.Execute(
+                sql: $@"
+EXECUTE [DMS].[BuildFolderPath] 
+   {AppId}
+  ,'test/foo/bar';
+
+EXECUTE [DMS].[BuildFolderPath] 
+   {AppId}
+  ,'test/bar';
+
+EXECUTE [DMS].[CreateFile] 
+   '{UserId}'
+  ,'test.txt'
+  ,'test/bar'
+  ,{AppId}
+  ,'text/plain'
+  ,'0 B'
+  ,0x2;
+
+EXECUTE [DMS].[CreateFile] 
+   '{UserId}'
+  ,'test.txt'
+  ,'test/foo/bar'
+  ,{AppId}
+  ,'text/plain'
+  ,'0 B'
+  ,0x3;", transaction: Transaction);
+
+            //when
+            Connection.Execute(sql: $@"
+EXECUTE [DMS].[MoveFolderToFolder] 
+   '{UserId}'
+  ,{AppId}
+  ,'test/bar'
+  ,'test/foo';",
+  transaction: Transaction);
+
+            var actualFolders = Connection.Query<Folder>($"SELECT * FROM [DMS].[Folders]", transaction: Transaction);
+            var actualFiles = Connection.Query<File>("SELECT * FROM [DMS].[Files]", transaction: Transaction);
+            var actualContents = Connection.Query<FileContent>("SELECT * FROM [DMS].[FileContents]", transaction: Transaction);
+
+            //then
+            Cleanup();
+
+            Assert.True(actualFolders.Count() == 3 
+                && actualFolders.Any(f => f.Path == "test") 
+                && actualFolders.Any(f => f.Path == "test/foo")
+                && actualFolders.Any(f => f.Path == "test/foo/bar"));
+
+            //Files should've been merged into one.
+            Assert.True(actualFiles.Count() == 1);
+
+            var contentFileIds = actualContents.Select(c => c.FileId).Distinct();
+
+            Assert.True(contentFileIds.Count() == 1);
+
+            //Verify contents
+            Assert.Contains(actualContents, c => c.Version == 1 && c.RawData[0] == 0x03);
+            Assert.Contains(actualContents, c => c.Version == 2 && c.RawData[0] == 0x02);
+        }
+    }
+}
